6ec7aaaaf0cfaaa79a693065a9256d936d9ed8b7
Oliver Götz <goetzoliver89@gmail.com>
2024-12-18 23:51:33 +0100

    image url adjusted

---
+++ infrastructure/generated_terraform.tf
@@ -0,0 +1,5 @@
+
+   locals {
+                             
+    ssm_parameters = ['/preprod/fit-mit-michi/next_public_clerk_publishable_key', '/preprod/fit-mit-michi/clerk_secret_key', '/preprod/fit-mit-michi/resend_api_key', '/preprod/fit-mit-michi/next_public_stripe_publishable_key', '/preprod/fit-mit-michi/stripe_api_secret_key', '/preprod/fit-mit-michi/stripe_webhook_secret', '/preprod/fit-mit-michi/sentry_auth_token']                          
+   }                           
