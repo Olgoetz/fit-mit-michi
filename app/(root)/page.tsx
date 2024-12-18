71373054546453c49bb08d58686b1a956e85245b
Oliver Götz <goetzoliver89@gmail.com>
2024-12-18 15:49:51 +0100

    auth removed

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,10 @@
 import AboutMe from "@/components/AboutMe";
-import UserAuthForm from "@/components/authentication/user-register-form";
+
 import ContactForm from "@/components/ContactForm";
 import Header from "@/components/Header";
 import Navbar from "@/components/Navbar";
 import Offer from "@/components/Offer";
 import Prices from "@/components/Prices";
-import { SignedOut, SignInButton, SignedIn, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   // console.log(process.env);
